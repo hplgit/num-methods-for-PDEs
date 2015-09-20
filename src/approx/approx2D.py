ae6a9a6d4f506d25853da88554eb19789840b39c
Hans Petter Langtangen <hpl@simula.no>
2015-09-20 12:06:36 -0700

    updates

--- src/approx/approx2D.py
+++ src/approx/approx2D.py
@@ -1,14 +1,12 @@
 """
 Approximation of functions by linear combination of basis functions in
-function spaces and the least squares method or the collocation method
-for determining the coefficients. 2D version.
+function spaces and the least squares method (or the Galerkin method).
+2D version.
 """
 import sympy as sym
-from scitools.std import surfc, savefig, linspace, title, figure, \
-     hot, axis, ndgrid, subplot
+import numpy as np
 
-
-def least_squares(f, psi, Omega, symbolic=True):
+def least_squares(f, psi, Omega, symbolic=True, print_latex=False):
     """
     Given a function f(x,y) on a rectangular domain
     Omega=[[xmin,xmax],[ymin,ymax]],
@@ -54,21 +52,23 @@ def least_squares(f, psi, Omega, symbolic=True):
     print 'A:\n', A, '\nb:\n', b
     if symbolic:
         c = A.LUsolve(b)  # symbolic solve
+        # c is a sympy Matrix object, numbers are in c[i,0]
+        c = [c[i,0] for i in range(c.shape[0])]
     else:
         c = sym.mpmath.lu_solve(A, b)  # numerical solve
     print 'coeff:', c
 
-    # c is a sympy Matrix object, numbers are in c[i,0]
-    u = sum(c[i,0]*psi[i] for i in range(len(psi)))
+    u = sum(c[i]*psi[i] for i in range(len(psi)))
     print 'approximation:', u
     print 'f:', sym.expand(f)
-    print sym.latex(A, mode='plain')
-    print sym.latex(b, mode='plain')
-    print sym.latex([c[i,0] for i in range(len(psi))], mode='plain')
-    return u
+    if print_latex:
+        print sym.latex(A, mode='plain')
+        print sym.latex(b, mode='plain')
+        print sym.latex(c, mode='plain')
+    return u, c
 
 
-def comparison_plot(f, u, Omega, plotfile='tmp'):
+def comparison_plot(f, u, Omega, plotfile='tmp', title=''):
     """Compare f(x,y) and u(x,y) for x,y in Omega in a plot."""
     x, y = sym.symbols('x y')
 
@@ -84,25 +84,37 @@ def comparison_plot(f, u, Omega, plotfile='tmp'):
                 Omega[r][s] = float(Omega[r][s].evalf())
 
     resolution = 41  # no of points in plot
-    xcoor = linspace(Omega[0][0], Omega[0][1], resolution)
-    ycoor = linspace(Omega[1][0], Omega[1][1], resolution)
-    xv, yv = ndgrid(xcoor, ycoor)
+    xcoor = np.linspace(Omega[0][0], Omega[0][1], resolution)
+    ycoor = np.linspace(Omega[1][0], Omega[1][1], resolution)
+    xv, yv = np.meshgrid(xcoor, ycoor)
     # Vectorized functions expressions does not work with
     # lambdify'ed functions without the modules="numpy"
     exact  = f(xv, yv)
     approx = u(xv, yv)
-    figure()
-    surfc(xv, yv, exact, title='f(x,y)',
-          colorbar=True, colormap=hot(), shading='flat')
+    error = exact - approx
+
+    import matplotlib.pyplot as plt
+    plt.figure()
+    contours = plt.contour(xv, yv, error, 8)     # 8 contour lines
+    plt.clabel(contours, inline=1, fontsize=10)  # labels
+    if title: plt.title(title)
     if plotfile:
-        savefig('%s_f.pdf' % plotfile, color=True)
-        savefig('%s_f.png' % plotfile)
-    figure()
-    surfc(xv, yv, approx, title='f(x,y)',
-          colorbar=True, colormap=hot(), shading='flat')
+        plt.savefig('%s_error_c.pdf' % plotfile)
+        plt.savefig('%s_error_c.png' % plotfile)
+    fig = plt.figure()
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+    #ax = fig.add_subplot(111) #, projection='3d')
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(xv, yv, error, rstride=1, cstride=1,
+                           cmap=cm.coolwarm, linewidth=0,
+                           antialiased=False)
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    if title: plt.title(title)
     if plotfile:
-        savefig('%s_u.pdf' % plotfile, color=True)
-        savefig('%s_u.png' % plotfile)
+        plt.savefig('%s_error_s.pdf' % plotfile)
+        plt.savefig('%s_error_s.png' % plotfile)
+    plt.show()
 
 if __name__ == '__main__':
     print 'Module file not meant for execution.'
